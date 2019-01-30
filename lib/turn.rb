b32d588f54feb2d0cf999954738628304d1b22ab
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-30 17:02:38 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+# pseudocode
+# ask for input
+# convert input to index
+#   if index is valid 
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
